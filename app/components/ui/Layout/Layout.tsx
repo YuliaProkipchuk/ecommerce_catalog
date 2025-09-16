b17e1a849e89c1db2c7906de972b789bf72bebd4
Oleksandr Mykyteichuk <psihanul@ukr.net>
2025-09-16 19:02:49 +0300

    feat: layout for products

---
+++ app/components/ui/Layout/Layout.tsx
@@ -0,0 +1,36 @@
+import { ProductCard } from "../ProductCard/ProductCard";
+import classes from './Layout.module.scss';
+
+export function Layout() {
+    return (
+        <section className='section'>
+        <div className={classes.layout}>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+        </div>
+        </section>
+    );
+}
