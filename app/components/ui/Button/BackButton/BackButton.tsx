d170f6a9834797f3ecd7e565cd8d39f88d80db9b
nesms <serhiy.nesmeyanov@qpard.com>
2025-09-16 18:26:40 +0300

    fix: hot fixes DREAMTEAM

--- app/components/ui/Button/BackButton/BackButton.tsx
+++ app/components/ui/Button/BackButton/BackButton.tsx
@@ -1,13 +1,12 @@
 'use client';
 import classes from './BackButton.module.scss';
 import Image from 'next/image';
-import ArrowLeft from '@/public/icons/ArrowLeftActive.svg';
 import { useRouter } from 'next/navigation';
 export function BackButton() {
   const router = useRouter();
   return (
     <div className={classes.back_btn} onClick={() => router.back()}>
-      <Image src={ArrowLeft} alt="arrow left icon" width={16} height={16} />
+      <Image src={'/icons/ArrowLeftActive.svg'} alt="arrow left icon" width={16} height={16} />
       <span>Back</span>
     </div>
   );
--- app/components/ui/CartItem/CartItem.tsx
+++ app/components/ui/CartItem/CartItem.tsx
@@ -1,25 +1,22 @@
 import classes from './CartItem.module.scss';
 import ItemImg from '../ProductCard/Image/Phone.png';
-import CloseIcon from '@/public/icons/Close.svg';
-import MinusIcon from '@/public/icons/Minus.svg';
-import PlusIcon from '@/public/icons/Plus.svg';
 import Image from 'next/image';
 export function CartItem() {
   return (
     <div className={classes.cart_item}>
       <button className={classes.close_btn}>
-        <Image src={CloseIcon} alt="x" width={16} height={16} />
+        <Image src={'/icons/Close.svg'} alt="x" width={16} height={16} />
       </button>
       <Image src={ItemImg} alt="item image" width={80} height={80} className={classes.image} />
       <p className={classes.title}>Apple iPhone 14 Pro 128GB Silver (MQ023)</p>
       <div className={classes.price_layout}>
         <div className={classes.quantityControls}>
           <button>
-            <Image src={MinusIcon} alt="-" width={16} height={16} />
+            <Image src={'/icons/Minus.svg'} alt="-" width={16} height={16} />
           </button>
           <span className={classes.amount}>1</span>
           <button>
-            <Image src={PlusIcon} alt="+" width={16} height={16} />
+            <Image src={'/icons/Plus.svg'} alt="+" width={16} height={16} />
           </button>
         </div>
 
