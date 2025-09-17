d850a6a0b151f351a3a722725ea24a8164042c67
Did Zhara <ruslankoval70@gmail.com>
2025-09-17 11:15:10 +0300

    refactoring: carousel refactored to have implemented buttons and showcase info from productCard

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,7 @@
 import { useTranslations } from 'next-intl';
 import { Header } from '../components/layout/Header/Header';
 import { ProductCastomization } from '../components/ui/ProductCastomization/ProductCastomization';
+import { ProductsCarousel } from '../components/layout/ProductsCarousel/ProductsCarousel';
 
 export default function Home() {
   const t = useTranslations();
@@ -9,7 +10,7 @@ export default function Home() {
     <main className={'main-container'}>
       <Header />
       {/* <p>{t('test')}</p> */}
-      <ProductCastomization />
+      <ProductsCarousel />
     </main>
   );
 }
--- app/components/layout/ProductsCarousel/ProductsCarousel.tsx
+++ app/components/layout/ProductsCarousel/ProductsCarousel.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import { Carousel } from "../../ui/Carousel/Carousel";
-import classes from "./ProductCarousel.module.scss";
+import React from 'react';
+import { Carousel } from '../../ui/Carousel/Carousel';
+import classes from './ProductCarousel.module.scss';
+import { LeftButton } from '../../ui/Button/LeftButton/LeftButton';
+import { RightButton } from '../../ui/Button/RightButton/RightButton';
 type Props = {
   title?: string;
 };
-export function ProductsCarousel({ title = "Brand new models" }: Props) {
+export function ProductsCarousel({ title = 'Brand new models' }: Props) {
   return (
     <section className="section">
       <div className={classes.carousel_header}>
         <h2 className="section-title">{title}</h2>
         <div className={classes.carousel_navigation}>
           {/* there will be buttons */}
-          <button className={`custom-prev`}>L</button>
-          <button className={`custom-next`}>R</button>
+          <div className="custom-prev">
+            <LeftButton />
+          </div>
+          <div className="custom-next">
+            <RightButton />
+          </div>
         </div>
       </div>
 
