109c07e8327db778552670974de116ffb96a8fe3
Oleksandr Mykyteichuk <psihanul@ukr.net>
2025-09-23 10:17:00 +0300

    add contacts page

---
+++ app/[locale]/(shop)/contacts/page.tsx
@@ -0,0 +1,12 @@
+import { ContactsPage } from "@/app/components/ContactsPage/ContactsPage";
+
+async function Page() {
+
+  return (
+    <>
+    <ContactsPage />
+    </>
+  );
+}
+
+export default Page;
