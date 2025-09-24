f9fe3cdc5d4eac672a226862eb2f190d7a061250
Oleksandr Mykyteichuk <psihanul@ukr.net>
2025-09-25 00:03:49 +0300

    create search fix

--- app/helpers/products/filterAndSort.ts
+++ app/helpers/products/filterAndSort.ts
@@ -1,9 +1,9 @@
 import { SortBy } from "@/app/stores/slices/productSlice";
 import { Product } from "@/app/types/product";
 
-export function prepareData(data:Product[], category:string, sortBy:SortBy) {
+export function prepareData(data:Product[], category:string, sortBy:SortBy, searchQuery:string) {
   return data
-    .filter((product) => product.category === category)
+    .filter((product) => product.category === category && product.name.toLowerCase().includes(searchQuery.toLowerCase()))
     .toSorted((p1, p2) => {
       const key = sortBy.param;
       const order = sortBy.order;
